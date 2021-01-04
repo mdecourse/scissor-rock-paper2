ca3ebd51cadbdf409bef33e16817cf76eee6b7cb
mdecourse <mdecourse@gmail.com>
2021-01-04 23:08:20 +0800

    use port 8080 for heroku deployment

--- wsgi.py
+++ wsgi.py
@@ -74,4 +74,5 @@ def check():
         return "no computer session"
          
   
-app.run(host='127.0.0.1', port=5000)
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
